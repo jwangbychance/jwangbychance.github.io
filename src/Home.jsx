7199c423f9cbeeba5c314d89997d44979c84c425
jwangbychance <jackson.wang1998@outlook.com>
2023-10-04 17:46:26 +1100

    add resume pdf download link and github profile pic

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,6 +21,10 @@ const Home = () => {
         >
           Hey, I'm Jackson!
         </h1>
+        <img
+          src="src/assets/p5_joker.png"
+          className="rounded-full w-28 md:w-60"
+        />
         <div>UTS student with a passion for Web Dev</div>
         <div>
           Check out my{" "}
@@ -33,7 +37,15 @@ const Home = () => {
             </span>
           </Link>
         </div>
-        {/* <div>Resume PDF download</div> */}
+        <a
+          href="src/assets/JacksonWang_Resume.pdf"
+          download
+          className="group font-semibold text-[#8b3dff] transition-all duration-300 ease-in-out"
+        >
+          <div className="bg-left-bottom bg-gradient-to-r from-[#8b3dff] to-[#8b3dff] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+            Resume PDF Download
+          </div>
+        </a>
       </div>
       <Footer />
     </div>
