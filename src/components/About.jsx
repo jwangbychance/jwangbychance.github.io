362e3b237d09f49c17187c93f3d6788f10780ba7
jwangbychance <jackson.wang1998@outlook.com>
2023-10-10 15:23:31 +1100

    update about me page to include logos and responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
   }, []);
 
   return (
-    <div className="font-open-sans w-full">
+    <div className="text-sm md:text-base font-open-sans w-full">
       <Navbar />
       <div
         className={`transition-all ${
@@ -19,43 +19,52 @@ const About = () => {
         } duration-500 ease-in-out`}
       >
         <div className="p-10">
-          <div className="text-4xl font-bold mb-10">About me</div>
-          <div>
-            Hey, I'm Jackson, an IT and business student at the University of
-            Technology Sydney.
-          </div>
-          <div className="mb-10">
-            I'm interested in web dev (both frontend and backend) and using
-            Node.js to program
-            <br />
-            my very own Discord bot for fun
-          </div>
-          <div className="mb-10">
-            I was given an opportunity to work at a hyper-growth startup company
-            that was accelerated
-            <br /> by the{" "}
-            <a
-              href="https://www.techstars.com/"
-              className="group text-[#8b3dff] transition-all duration-300 ease-in-out"
-            >
-              <span className="bg-left-bottom bg-gradient-to-r from-[#8b3dff] to-[#8b3dff] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-                Techstars
-              </span>
-            </a>
-            &nbsp;program as part of a small, dynamic team where I made
-            meaningful contributions <br /> while continuing to grow with the
-            company
-          </div>
-          <div>
-            Check out my contributions{" "}
-            <Link
-              to="../projects"
-              className="group text-[#8b3dff] transition-all duration-300 ease-in-out"
-            >
-              <span className="bg-left-bottom bg-gradient-to-r from-[#8b3dff] to-[#8b3dff] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-                here!
-              </span>
-            </Link>
+          <div className="mb-2 md:text-4xl font-bold md:mb-10">About me</div>
+          <div className="md:flex">
+            <div className="md:w-4/12 flex flex-col">
+              <div className="mb-5">
+                Hey, I'm Jackson, an IT and business student at the University
+                of Technology Sydney.
+              </div>
+              <div className="mb-5">
+                I'm interested in web dev (both frontend and backend) and using
+                Node.js to program my very own Discord bot for fun.
+              </div>
+              <div className="mb-5">
+                I was given an opportunity to work at a hyper-growth startup
+                company that was accelerated by the{" "}
+                <a
+                  href="https://www.techstars.com/"
+                  className="group text-[#8b3dff] transition-all duration-300 ease-in-out"
+                >
+                  <span className="bg-left-bottom bg-gradient-to-r from-[#8b3dff] to-[#8b3dff] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                    Techstars
+                  </span>
+                </a>
+                &nbsp;program as part of a small, dynamic team where I made
+                meaningful contributions while continuing to grow with the
+                company.
+              </div>
+              <div>
+                Check out my contributions{" "}
+                <Link
+                  to="../projects"
+                  className="group text-[#8b3dff] transition-all duration-300 ease-in-out"
+                >
+                  <span className="bg-left-bottom bg-gradient-to-r from-[#8b3dff] to-[#8b3dff] bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                    here!
+                  </span>
+                </Link>
+              </div>
+            </div>
+            <div className="flex md:block mt-5 md:-mt-20 md:ml-auto md:mr-20 md:w-fit">
+              <div>
+                <img src="/assets/geckorent.png" alt="Logo of Gecko.rent" />
+              </div>
+              <div>
+                <img src="/assets/techstars.png" alt="Logo of Techstars" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
