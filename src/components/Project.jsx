46a152226018327979dfba51fd6f8455029a7b36
jwangbychance <jackson.wang1998@outlook.com>
2023-10-12 15:13:25 +1100

    adjust responsiveness of project popup designs on mobile

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,10 +9,12 @@ const Project = ({ title, content, tech, src, showModal, toggleModal }) => {
       closeTimeoutMS={500}
       className="border-2 border-[#d3b6ff] rounded-xl p-5 h-[90%] mx-auto top-10 w-[90%] relative bg-white"
     >
-      <div className="font-bold text-2xl mb-8 text-[#8b3dff]">{title}</div>
+      <div className="font-bold text-lg mb-3 md:text-2xl md:mb-8 text-[#8b3dff]">
+        {title}
+      </div>
       <button
         onClick={toggleModal}
-        className="text-black w-8 absolute top-3 right-3"
+        className="text-black w-5 md:w-8 absolute top-3 right-3"
       >
         <svg
           fill="none"
@@ -29,13 +31,21 @@ const Project = ({ title, content, tech, src, showModal, toggleModal }) => {
           />
         </svg>
       </button>
-      <div className="font-semibold mb-5">Technologies: {tech}</div>
-      <div className="mb-10">{content}</div>
+      <div className="font-semibold mb-5 text-sm md:text-base">
+        Technologies: {tech}
+      </div>
+      <div className="mb-10 text-sm md:text-base">{content}</div>
       <div className="flex justify-center">
         {src.endsWith("png") ? (
           <img src={src} width={1200} />
         ) : (
-          <iframe controls src={src} width={1020} height={580} />
+          <iframe
+            controls
+            src={src}
+            width={1020}
+            height={580}
+            allowFullScreen
+          />
         )}
       </div>
     </Modal>
