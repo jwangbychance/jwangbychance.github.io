a171b8a1b31c9305416912c8c212dbe4ed66b687
jwangbychance <jackson.wang1998@outlook.com>
2023-12-05 11:51:37 +1100

    adjust positions of navbar logos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="font-open-sans text-sm w-full flex justify-center md:gap-3 px-3 border border-b-gray-200 shadow-md">
       <img
-        className="absolute left-2 h-10 md:h-14 self-center"
+        className="absolute left-1 h-10 md:h-14 self-center"
         src="/assets/logo.png"
       ></img>
       <div className="ml-auto md:ml-0 py-1 px-2 md:px-3 md:py-2 flex md:gap-3 md:text-sm bg-[#F0F0F0] shadow-sm my-2 rounded-full text-xs">
@@ -15,7 +15,7 @@ const Navbar = () => {
         <CustomNavLink to="/about">About</CustomNavLink>
         <CustomNavLink to="/projects">Projects</CustomNavLink>
       </div>
-      <div className="hidden md:flex absolute right-2 self-center w-28 gap-3">
+      <div className="hidden md:flex absolute right-3 self-center w-28 gap-3">
         <TypeScriptSVG />
         <ReactSVG />
         <TailwindCSS />
